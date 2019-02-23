da5c0ac5260b53857547783729025b94ad0c21ea
atlansien <atlansien@yahoo.co.jp>
2019-02-23 10:33:00 +0900

    add js exercise 14

--- main.js
+++ main.js
@@ -7,10 +7,24 @@
 //   - height: 身長
 //   - weight: 体重
 // - インスタンスごとの固有の情報はコンストラクタ経由でセットする
+class Person {
+    constructor(name, age, gender, height, weight){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+    }
 
+};
 
 // 課題2: 自分もしくは、架空の人の情報を使って１人の人間を表すPersonインスタンスを生成する
 // - インスタンスは変数 `me` に代入する
-
+const me = new Person('じん', 24, '男', 175, 64);
 
 // 課題3: 課題2で生成したインスタンスの情報をconsole.logを使って、「名前」「年齢」「性別」「身長」「体重」が正しくセットされていることを確認する
+console.log('me.nameはじんであるべき : ', me.name);
+console.log('me.ageは24であるべき : ', me.age);
+console.log('me.genderは男であるべき : ', me.gender);
+console.log('me.heightは175であるべき : ', me.height);
+console.log('me.weightは64であるべき : ', me.weight);
